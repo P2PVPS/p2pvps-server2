48e73461651305e31f626f0f65db5a19fce5ca3c
Chris Troutner <chris.troutner@gmail.com>
2018-03-09 14:14:01 -0800

    POST /device creates both a public and private data model.

---
+++ src/models/deviceprivatedata.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const DevicePrivateData = new mongoose.Schema({
+  ownerUser: { type: String, required: true },
+  publicData: { type: String, required: true },
+  renterUser: { type: String },
+  serverSSHPort: { type: String },
+  deviceUserName: { type: String },
+  devicePassword: { type: String },
+  jumperState: {type: Boolean},
+  moneyPending: { type: Number },
+  moneyOwed: { type: Number }
+})
+
+module.exports = mongoose.model('devicePrivateModel', DevicePrivateData)
