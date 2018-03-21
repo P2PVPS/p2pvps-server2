813d12ccf26bcc7eaecd095f8f555a0e26edd36e
Chris Troutner <chris.troutner@gmail.com>
2018-03-21 15:38:13 -0700

    Added utility functions

---
+++ util/devices/getDevices.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const config = require('../../config')
+
+// Connect to the Mongo Database.
+mongoose.Promise = global.Promise
+mongoose.connect(config.database)
+
+const Device = require('../../src/models/devicepublicdata')
+
+async function getDevices () {
+  const devices = await Device.find({})
+  console.log(`devices: ${JSON.stringify(devices, null, 2)}`)
+
+  mongoose.connection.close()
+}
+getDevices()
+
+module.exports = {
+  getDevices
+}
--- util/users/getUsers.js
+++ util/users/getUsers.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose')
 
-const config = require('../config')
+const config = require('../../config')
 
 // Connect to the Mongo Database.
 mongoose.Promise = global.Promise
 mongoose.connect(config.database)
 
-const User = require('../src/models/users')
+const User = require('../../src/models/users')
 
 async function getUsers () {
   const users = await User.find({}, '-password')
   console.log(`users: ${JSON.stringify(users, null, 2)}`)
+
+  mongoose.connection.close()
 }
 getUsers()
