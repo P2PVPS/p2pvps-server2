15520d7c4abf329a764bc2497feb9a1ff708992e
Chris Troutner <chris.troutner@gmail.com>
2018-03-24 08:15:44 -0700

    Changing mongodb port to reflect server deployment

---
+++ config/env/development.js
@@ -0,0 +1,14 @@
+/*
+export default {
+  session: 'secret-boilerplate-token',
+  token: 'secret-jwt-token',
+  database: 'mongodb://localhost:27017/p2pvps-server-dev'
+}
+*/
+
+module.exports = {
+  session: 'secret-boilerplate-token',
+  token: 'secret-jwt-token',
+  // database: 'mongodb://localhost:27017/p2pvps-server-dev'
+  database: 'mongodb://localhost:3500/p2pvps-server-dev'
+}
