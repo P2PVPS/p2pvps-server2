6df6009afe54c3c20fcced7f88547f8683b8d1ca
Chris Troutner <chris.troutner@gmail.com>
2018-03-29 20:36:34 -0700

    Removed unneeded comments

--- test/a02-user.spec.js
+++ test/a02-user.spec.js
@@ -1,7 +1,3 @@
-const app = require('../bin/server')
-// const supertest = require('supertest')
-// const { expect, should } = require('chai')
-const expect = require('chai').expect
 const should = require('chai').should
 const cleanDb = require('./utils').cleanDb
 const serverUtil = require('../bin/util')
@@ -28,12 +24,6 @@ describe('Users', () => {
 
   describe('POST /users', () => {
     it('should reject signup when data is incomplete', async () => {
-      // request
-      //  .post('/users')
-      //  .set('Accept', 'application/json')
-      //  .send({ username: 'supercoolname' })
-      //  .expect(422, done)
-
       try {
         const options = {
           method: 'POST',
@@ -95,30 +85,6 @@ describe('Users', () => {
         console.log('Error stringified: ' + JSON.stringify(err, null, 2))
         throw err
       }
-      /*
-      request
-        .post('/users')
-        .set('Accept', 'application/json')
-        .send({ user: { username: 'supercoolname', password: 'supersecretpassword', type: 'blah' } })
-        .expect(200, (err, res) => {
-          if (err) { return done(err) }
-          // console.log(`res.body: ${JSON.stringify(res.body, null, 2)}`)
-
-          res.body.user.should.have.property('username')
-          res.body.user.username.should.equal('supercoolname')
-
-          res.body.user.should.have.property('type')
-          res.body.user.type.should.equal('user')
-
-          expect(res.body.user.password).to.not.exist
-
-          context.user = res.body.user
-          context.token = res.body.token
-          // console.log(`token: ${res.body.token}`)
-
-          done()
-        })
-        */
     })
   })
 
@@ -147,12 +113,6 @@ describe('Users', () => {
           throw err
         }
       }
-      /*
-      request
-        .get('/users')
-        .set('Accept', 'application/json')
-        .expect(401, done)
-      */
     })
 
     it('should not fetch users if the authorization header is missing the scheme', async () => {
@@ -182,15 +142,6 @@ describe('Users', () => {
           throw err
         }
       }
-      /*
-      request
-        .get('/users')
-        .set({
-          Accept: 'application/json',
-          Authorization: '1'
-        })
-        .expect(401, done)
-      */
     })
 
     it('should not fetch users if the authorization header has invalid scheme', async () => {
@@ -220,17 +171,6 @@ describe('Users', () => {
           throw err
         }
       }
-
-      /*
-      const { token } = context
-      request
-        .get('/users')
-        .set({
-          Accept: 'application/json',
-          Authorization: `Unknown ${token}`
-        })
-        .expect(401, done)
-      */
     })
 
     it('should not fetch users if token is invalid', async () => {
@@ -260,15 +200,6 @@ describe('Users', () => {
           throw err
         }
       }
-      /*
-      request
-        .get('/users')
-        .set({
-          Accept: 'application/json',
-          Authorization: 'Bearer 1'
-        })
-        .expect(401, done)
-      */
     })
 
     it('should fetch all users', async () => {
@@ -294,25 +225,6 @@ describe('Users', () => {
         console.log('Error stringified: ' + JSON.stringify(err, null, 2))
         throw err
       }
-
-      /*
-      const { token } = context
-      request
-        .get('/users')
-        .set({
-          Accept: 'application/json',
-          Authorization: `Bearer ${token}`
-        })
-        .expect(200, (err, res) => {
-          if (err) { return done(err) }
-          // console.log(`Users: ${JSON.stringify(res.body, null, 2)}`)
-          res.body.should.have.property('users')
-
-          res.body.users.should.have.length(1)
-
-          done()
-        })
-        */
     })
   })
 
@@ -344,16 +256,6 @@ describe('Users', () => {
           throw err
         }
       }
-
-      /*
-      request
-        .get('/users/1')
-        .set({
-          Accept: 'application/json',
-          Authorization: 'Bearer 1'
-        })
-        .expect(401, done)
-      */
     })
 
     it('should throw 404 if user doesn\'t exist', async () => {
@@ -373,11 +275,6 @@ describe('Users', () => {
         console.log(`result stringified: ${JSON.stringify(result, null, 2)}`)
         assert(false, 'Unexpected result')
       } catch (err) {
-        // if (err.statusCode === 422) {
-        //  assert(err.statusCode === 422, 'Error code 422 expected.')
-        // } else if (err.statusCode === 401) {
-        //  assert(err.statusCode === 401, 'Error code 401 expected.')
-        // } else
         if (err.statusCode === 404) {
           assert(err.statusCode === 404, 'Error code 404 expected.')
         } else {
@@ -386,17 +283,6 @@ describe('Users', () => {
           throw err
         }
       }
-
-      /*
-      const { token } = context
-      request
-        .get('/users/1')
-        .set({
-          Accept: 'application/json',
-          Authorization: `Bearer ${token}`
-        })
-        .expect(404, done)
-      */
     })
 
     it('should fetch user', async () => {
@@ -423,31 +309,6 @@ describe('Users', () => {
         console.log('Error stringified: ' + JSON.stringify(err, null, 2))
         throw err
       }
-
-      /*
-      const {
-        user: { _id },
-        token
-      } = context
-
-      request
-        .get(`/users/${_id}`)
-        .set({
-          Accept: 'application/json',
-          Authorization: `Bearer ${token}`
-        })
-        .expect(200, (err, res) => {
-          if (err) { return done(err) }
-
-          // console.log(`res: ${JSON.stringify(res, null, 2)}`)
-
-          res.body.should.have.property('user')
-
-          expect(res.body.user.password).to.not.exist
-
-          done()
-        })
-      */
     })
   })
 
@@ -477,16 +338,6 @@ describe('Users', () => {
           throw err
         }
       }
-
-      /*
-      request
-        .put('/users/1')
-        .set({
-          Accept: 'application/json',
-          Authorization: 'Bearer 1'
-        })
-        .expect(401, done)
-      */
     })
 
     it('should throw 404 if user doesn\'t exist', async () => {
@@ -514,17 +365,6 @@ describe('Users', () => {
           throw err
         }
       }
-
-      /*
-      const { token } = context
-      request
-        .put('/users/1')
-        .set({
-          Accept: 'application/json',
-          Authorization: `Bearer ${token}`
-        })
-        .expect(404, done)
-      */
     })
 
     it('should update user', async () => {
@@ -556,29 +396,6 @@ describe('Users', () => {
         console.log('Error stringified: ' + JSON.stringify(err, null, 2))
         throw err
       }
-      /*
-      const {
-        user: { _id },
-        token
-      } = context
-
-      request
-        .put(`/users/${_id}`)
-        .set({
-          Accept: 'application/json',
-          Authorization: `Bearer ${token}`
-        })
-        .send({ user: { username: 'updatedcoolname' } })
-        .expect(200, (err, res) => {
-          if (err) { return done(err) }
-
-          res.body.user.should.have.property('username')
-          res.body.user.username.should.equal('updatedcoolname')
-          expect(res.body.user.password).to.not.exist
-
-          done()
-        })
-      */
     })
   })
 
@@ -608,15 +425,6 @@ describe('Users', () => {
           throw err
         }
       }
-      /*
-      request
-        .delete('/users/1')
-        .set({
-          Accept: 'application/json',
-          Authorization: 'Bearer 1'
-        })
-        .expect(401, done)
-      */
     })
 
     it('should throw 404 if user doesn\'t exist', async () => {
@@ -644,16 +452,6 @@ describe('Users', () => {
           throw err
         }
       }
-      /*
-      const { token } = context
-      request
-        .delete('/users/1')
-        .set({
-          Accept: 'application/json',
-          Authorization: `Bearer ${token}`
-        })
-        .expect(404, done)
-      */
     })
 
     it('should delete user', async () => {
@@ -683,20 +481,6 @@ describe('Users', () => {
         console.log('Error stringified: ' + JSON.stringify(err, null, 2))
         throw err
       }
-      /*
-      const {
-        user: { _id },
-        token
-      } = context
-
-      request
-        .delete(`/users/${_id}`)
-        .set({
-          Accept: 'application/json',
-          Authorization: `Bearer ${token}`
-        })
-        .expect(200, done)
-      */
     })
   })
 })
