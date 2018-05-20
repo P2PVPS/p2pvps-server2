7764a3cf41f6b5413346eec78108a7e4b1dade73
Chris Troutner <chris.troutner@gmail.com>
2018-05-20 14:38:21 -0700

    Removed tests not needed for open GET /api/devices endpoint

--- src/middleware/validators.js
+++ src/middleware/validators.js
@@ -65,7 +65,45 @@ async function ensureAdmin (ctx, next) {
   return next()
 }
 
+// This middleware ensures that the :id used in the API endpoint matches the
+// the ID used in the JWT, or failing that, the ID used in the JWT matches
+// an Admin user. This prevents situations like users updating other users
+// profiles or non-admins deleting users.
+// TODO Tests must be developed before developing this function.
+async function ensureTargetUserOrAdmin (ctx, next) {
+  // console.log(`getToken: ${typeof (getToken)}`)
+  const token = getToken(ctx)
+
+  if (!token) {
+    // console.log(`Err: Token not provided.`)
+    ctx.throw(401)
+  }
+
+  let decoded = null
+  try {
+    // console.log(`token: ${JSON.stringify(token, null, 2)}`)
+    // console.log(`config: ${JSON.stringify(config, null, 2)}`)
+    decoded = jwt.verify(token, config.token)
+  } catch (err) {
+    // console.log(`Err: Token could not be decoded: ${err}`)
+    ctx.throw(401)
+  }
+
+  ctx.state.user = await User.findById(decoded.id, '-password')
+  if (!ctx.state.user) {
+    // console.log(`Err: Could not find user.`)
+    ctx.throw(401)
+  }
+
+  if (ctx.state.user.type !== 'admin') {
+    ctx.throw(401, 'not admin')
+  }
+
+  return next()
+}
+
 module.exports = {
   ensureUser,
-  ensureAdmin
+  ensureAdmin,
+  ensureTargetUserOrAdmin
 }
