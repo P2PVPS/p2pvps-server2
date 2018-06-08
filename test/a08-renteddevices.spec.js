bdf1037619da322ee458607486365763878f6df2
Chris Troutner <chris.troutner@gmail.com>
2018-06-07 17:37:05 -0700

    Several tests created

--- test/a08-renteddevices.spec.js
+++ test/a08-renteddevices.spec.js
@@ -38,7 +38,7 @@ describe('Rented Devices', () => {
     const device = await utils.createDevice({token: context.userToken})
     context.device = device
 
-    //console.log(`context: ${JSON.stringify(context, null, 2)}`)
+    // console.log(`context: ${JSON.stringify(context, null, 2)}`)
   })
 
   describe('POST /renteddevices', () => {
@@ -96,7 +96,87 @@ describe('Rented Devices', () => {
     })
   })
 
-/*
+  describe('GET /renteddevices', () => {
+    it('should add device ID to the rentedDevices list', async () => {
+      try {
+        const options = {
+          method: 'GET',
+          uri: `${LOCALHOST}/api/renteddevices`,
+          resolveWithFullResponse: true,
+          json: true
+        }
+
+        let result = await rp(options)
+
+        assert(result.statusCode === 200, 'Status Code 200 expected.')
+        assert.isArray(result.body.devices, 'Returns an array')
+      } catch (err) {
+        console.error(err)
+        assert(false, 'Unexpected result')
+      }
+    })
+  })
+
+  describe('DELETE /renteddevices', () => {
+    it('should throw 404 if device is not in list', async () => {
+      try {
+        const options = {
+          method: 'DELETE',
+          uri: `${LOCALHOST}/api/renteddevices/123`,
+          resolveWithFullResponse: true,
+          json: true
+        }
+
+        let result = await rp(options)
+
+        console.log(`result stringified: ${JSON.stringify(result, null, 2)}`)
+        assert(false, 'Unexpected result')
+      } catch (err) {
+        // console.error(`err: ${JSON.stringify(err, null, 2)}`)
+        assert(err.statusCode === 404, 'Returns HTTP status 404.')
+      }
+    })
+
+    it('should delete the device from the list', async () => {
+      try {
+        const options = {
+          method: 'DELETE',
+          uri: `${LOCALHOST}/api/renteddevices/${context.device._id}`,
+          resolveWithFullResponse: true,
+          json: true
+        }
+
+        let result = await rp(options)
+
+        // console.log(`result stringified: ${JSON.stringify(result, null, 2)}`)
+        assert(result.statusCode === 200, 'Status Code 200 expected.')
+        assert(result.body.success === true, 'Success exected.')
+      } catch (err) {
+        console.error(`err: ${JSON.stringify(err, null, 2)}`)
+        assert(false, 'Unxpected result')
+      }
+    })
+
+    it('should throw 404 if list is empty', async () => {
+      try {
+        const options = {
+          method: 'DELETE',
+          uri: `${LOCALHOST}/api/renteddevices/${context.device._id}`,
+          resolveWithFullResponse: true,
+          json: true
+        }
+
+        let result = await rp(options)
+
+        console.log(`result stringified: ${JSON.stringify(result, null, 2)}`)
+        assert(false, 'Unexpected result')
+      } catch (err) {
+        // console.error(`err: ${JSON.stringify(err, null, 2)}`)
+        assert(err.statusCode === 404, 'Returns HTTP status 404.')
+      }
+    })
+  })
+
   describe('GET /api/renteddevices/renew/:id', () => {
     it('If the rentedDevices list is empty, returns 422', async () => {
       try {
@@ -113,12 +193,55 @@ describe('Rented Devices', () => {
         assert(false, 'Unexpected result')
       } catch (err) {
         assert(err.statusCode === 422, 'Returns HTTP status 422.')
+      }
+    })
 
-        console.log(`This continues to log`)
+    it('If the device ID is not in the rentedDevices list, returns 422', async () => {
+      try {
+        // Create a device
+        const newDevice = await utils.createDevice({token: context.userToken})
 
-        // TODO Add nome non-existant device ID to the list, so that it's not empty.
+        // Add device ID to the list, so that it's not empty.
+        await utils.addDeviceToRentedList(newDevice._id)
+
+        context.device2 = newDevice
+
+        const options = {
+          method: 'GET',
+          uri: `${LOCALHOST}/api/renteddevices/renew/${context.device._id}`,
+          resolveWithFullResponse: true,
+          json: true
+        }
+
+        let result = await rp(options)
+
+        console.log(`result stringified: ${JSON.stringify(result, null, 2)}`)
+        assert(false, 'Unexpected result')
+      } catch (err) {
+        assert(err.statusCode === 422, 'Returns HTTP status 422.')
+      }
+    })
+
+    it('Renews the device.', async () => {
+      try {
+        // Add the device ID to the list, so that it's not empty.
+        await utils.addDeviceToRentedList(context.device._id)
+
+        const options = {
+          method: 'GET',
+          uri: `${LOCALHOST}/api/renteddevices/renew/${context.device._id}`,
+          resolveWithFullResponse: true,
+          json: true
+        }
+
+        let result = await rp(options)
+
+        assert(result.statusCode === 200, 'Status Code 200 expected.')
+        assert(result.body.success === true, 'Success exected.')
+      } catch (err) {
+        console.log(`result stringified: ${JSON.stringify(result, null, 2)}`)
+        assert(false, 'Unexpected result')
       }
     })
   })
-  */
 })
